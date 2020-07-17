43c5cc8fd031db39036aacc83c1a9039d75aeb95
humberd <humberd2@gmail.com>
2020-07-17 20:19:43 +0200

    Added sprint-details.component.html details table

--- src/app/views/sprint-details/sprint-details.component.ts
+++ src/app/views/sprint-details/sprint-details.component.ts
@@ -1,16 +1,67 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { SprintDomainService } from '../../domain/entities/sprint/sprint-domain.service';
+import { BugfixDomainService } from '../../domain/entities/bugfix/bugfix-domain.service';
+import { SprintView } from '../../domain/entities/sprint/view/sprint-view';
+import { ActivatedRoute } from '@angular/router';
+import { defaultViewList, ViewList } from '../../domain/common/view-list';
+import { BugfixView } from '../../domain/entities/bugfix/view/bugfix-view';
 
 @Component({
   selector: 'app-sprint-details',
   templateUrl: './sprint-details.component.html',
   styleUrls: ['./sprint-details.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SprintDetailsComponent implements OnInit {
+  displayedColumns = ['name', 'ticket', 'bugPreview', 'fixPreview', 'actions'];
+  sprintView: SprintView;
+  items: ViewList<BugfixView> = defaultViewList();
 
-  constructor() { }
+  private sprintId: string;
 
-  ngOnInit(): void {
+  constructor(
+    private sprintDomainService: SprintDomainService,
+    private bugfixDomainService: BugfixDomainService,
+    private activatedRoute: ActivatedRoute,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) {
+    this.sprintId = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  ngOnInit(): void {
+    this.fetchSprintDetails();
+    this.fetchBugfixes();
+  }
+
+  typeIt(row: BugfixView): BugfixView {
+    return row;
+  }
+
+  private fetchSprintDetails() {
+    this.sprintDomainService.read(this.sprintId)
+      .subscribe(result => {
+        this.sprintView = result;
+        this.changeDetectorRef.markForCheck();
+      });
+  }
+
+  private fetchBugfixes() {
+    this.sprintDomainService.readBugfixesList(this.sprintId)
+      .subscribe(result => {
+        this.items = result;
+        this.changeDetectorRef.markForCheck();
+      });
+  }
+
+  async openUpdateSprintDialog(bugfixView: BugfixView) {
+
+  }
+
+  async openDeleteSprintDialog(bugfixView: BugfixView) {
+
+  }
+
+  async openNewSprintDialog() {
+
+  }
 }
--- src/app/views/sprint-details/sprint-details.module.ts
+++ src/app/views/sprint-details/sprint-details.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SprintDetailsComponent } from './sprint-details.component';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [SprintDetailsComponent],
@@ -13,6 +18,11 @@ import { RouterModule } from '@angular/router';
         component: SprintDetailsComponent,
       },
     ]),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatMenuModule,
   ],
 })
 export class SprintDetailsModule {
