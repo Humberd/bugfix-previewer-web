64cba05fe907093413fcb66ba3a5431c86e29a4e
humberd <humberd2@gmail.com>
2020-07-18 12:53:25 +0200

    Added demo.component.html

--- src/app/views/demo/demo.component.ts
+++ src/app/views/demo/demo.component.ts
@@ -1,20 +1,128 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SprintDomainService } from '../../domain/entities/sprint/sprint-domain.service';
+import { SprintView } from '../../domain/entities/sprint/view/sprint-view';
+import { BugfixView } from '../../domain/entities/bugfix/view/bugfix-view';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent implements OnInit, AfterViewInit, OnDestroy {
   private sprintId: string;
+  sprintView: SprintView;
+  bugfixes: BugfixView[] = [];
+  currentBugfixIndex = 0;
+  isBugPreview = true;
+  allEnded = false;
+  bugfixEnded = false;
+  bugPreviewEnded = false;
+  fixPreviewEnded = false;
+  isPlaying = false;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private sprintDomainService: SprintDomainService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) {
     this.sprintId = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
+    this.fetchSprintDetails();
+    this.fetchBugfixes();
   }
 
+  async ngAfterViewInit() {
+    try {
+      const response = await document.documentElement.requestFullscreen();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  ngOnDestroy() {
+    document.exitFullscreen();
+  }
+
+  getCurrentUrl(): string {
+    if (this.isBugPreview) {
+      return this.bugfixes[this.currentBugfixIndex].bugPreviewUrl;
+    }
+
+    return this.bugfixes[this.currentBugfixIndex].fixPreviewUrl;
+  }
+
+  private fetchSprintDetails() {
+    this.sprintDomainService.read(this.sprintId)
+      .subscribe({
+        next: result => {
+          this.sprintView = result;
+          this.changeDetectorRef.markForCheck();
+        },
+        error: error => {
+          this.snackBar.open(`There is no sprint ${this.sprintId}`);
+          this.router.navigateByUrl('/');
+        },
+      });
+  }
+
+  private fetchBugfixes() {
+    this.sprintDomainService.readBugfixesList(this.sprintId)
+      .subscribe(result => {
+        this.bugfixes = result.data.filter(it => it.bugPreviewUrl && it.fixPreviewUrl);
+        this.changeDetectorRef.markForCheck();
+      });
+  }
+
+  videoEnded() {
+    this.isPlaying = false;
+    if (this.isBugPreview) {
+      this.bugPreviewEnded = true;
+      this.fixPreviewEnded = false;
+
+      return;
+    }
+
+    this.bugPreviewEnded = false;
+    this.fixPreviewEnded = true;
+
+    if (this.currentBugfixIndex === this.bugfixes.length - 1) {
+      this.allEnded = true;
+
+      return;
+    }
+
+    this.bugfixEnded = true;
+  }
+
+  videoStarted() {
+    console.log('video started');
+    this.isPlaying = true;
+  }
+
+  videoPaused() {
+    this.isPlaying = false;
+  }
+
+  showFixPreview() {
+    this.isBugPreview = false;
+  }
+
+  showBugPreview() {
+    this.isBugPreview = true;
+  }
+
+  goToNextBugfix() {
+
+  }
+
+  goToPreviousBugfix() {
+
+  }
 }
--- src/app/views/demo/demo.module.ts
+++ src/app/views/demo/demo.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoComponent } from './demo.component';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [DemoComponent],
@@ -13,6 +15,8 @@ import { RouterModule } from '@angular/router';
         component: DemoComponent,
       },
     ]),
+    MatIconModule,
+    MatButtonModule,
   ],
 })
 export class DemoModule {
